2556afbecbb64b50a74366dc1501aa135d44f698
Hein Tonny Køien <hein.tonny@koien.no>
2024-12-02 00:27:31 +0100

    Test again

--- terraform/github_files.tf
+++ terraform/github_files.tf
@@ -1,5 +1,5 @@
 locals {
-  files_path = "${path.module}/files"
+  files_path = "${path.module}/../files"
 
   # Get all files recursively in the files directory
   all_files = fileset(local.files_path, "**/*")
@@ -16,10 +16,10 @@ locals {
 resource "github_repository_file" "ga_workflow_main_updates" {
   for_each            = local.file_map
   repository          = "terraform-azurerm-avm-res-documentdb-databaseaccount"
-  branch              = "main" # or whichever branch you want to add the file to
+  branch              = "main"
   file                = each.key
   content             = base64decode(each.value)
-  commit_message      = "[AVMUpgrades] Add GitHub Action workflow to update main branch"
+  commit_message      = "[AVMUpgrades] Update repository files"
   commit_author       = "Terraform"
   commit_email        = local.commit_email
   overwrite_on_create = true
---
+++ terraform/output.tf
@@ -0,0 +1,7 @@
+output "debug_file_map" {
+  value = local.file_map
+}
+
+output "debug_all_files" {
+  value = local.all_files
+}
