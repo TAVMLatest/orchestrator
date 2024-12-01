b674c9a399ffd10bb28abdc557db1b5e62e193e2
Hein Tonny Køien <hein.tonny@koien.no>
2024-12-01 21:56:42 +0100

    Add new workflow to one repo terraform-azurerm-avm-res-documentdb-databaseaccount

--- terraform/github_files.tf
+++ terraform/github_files.tf
@@ -5,10 +5,14 @@ locals {
 resource "github_repository_file" "ga_workflow_main_updates" {
   repository          = "terraform-azurerm-avm-res-documentdb-databaseaccount"
   branch              = "main"  # or whichever branch you want to add the file to
-  file                = "../files/avm_upgrades_to_main.yml"
+  file                = file("${path.module}/../files/avm_upgrades_to_main.yml")
   content             = "This is a new GitHub Action workflow for AVPUpgrades to main branch"
   commit_message      = "[AVMUpgrades] Add GitHub Action workflow to update main branch"
   commit_author       = "Terraform"
   commit_email        = local.commit_email
   overwrite_on_create = true
+}
+
+locals {
+  commit_email = "terraform@orchestrator.avmupgrades"
 }
