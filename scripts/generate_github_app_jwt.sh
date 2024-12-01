10c27dd03318d80bad03b02903c9a6e9ab7223eb
Hein Tonny Køien <hein.tonny@koien.no>
2024-12-01 13:47:51 +0100

    Test deploy

--- scripts/generate_github_app_jwt.sh
+++ scripts/generate_github_app_jwt.sh
@@ -9,6 +9,8 @@ fi
 
 # Generate the JWT token
 JWT_TOKEN=$(ruby -r openssl -r base64 -r json -e '
+  require "jwt"
+
   iat = Time.now.to_i
   exp = iat + (10 * 60)
   payload = {
@@ -16,7 +18,7 @@ JWT_TOKEN=$(ruby -r openssl -r base64 -r json -e '
     exp: exp,
     iss: ENV["GITHUB_APP_ID"]
   }
-  private_key = OpenSSL::PKey::RSA.new(ENV["GITHUB_APP_PRIVATE_KEY"])
+  private_key = OpenSSL::PKey::RSA.new(Base64.decode64(ENV["GITHUB_APP_PRIVATE_KEY"]))
   token = JWT.encode(payload, private_key, "RS256")
   puts token
 ')
