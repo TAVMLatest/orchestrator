d726ed6eabf1f64970e4cd2eac4c803a44001a47
Hein Tonny Køien <hein.tonny@koien.no>
2024-12-02 00:09:37 +0100

    Test again

--- terraform/github_files.tf
+++ terraform/github_files.tf
@@ -1,12 +1,24 @@
 locals {
-  # Concat with repos.json name
-  commit_email = "terraform@orchestrator.avmupgrades"
+  files_path = "${path.module}/files"
+
+  # Get all files recursively in the files directory
+  all_files = fileset(local.files_path, "**/*")
+
+  # Create a map of file paths to their content, removing the "files/" prefix
+  file_map = {
+    for file in local.all_files :
+    trimprefix(file, "files/") => filebase64("${local.files_path}/${file}")
+  }
+
+  commit_email = "terraform@updatedfiles.devops"
 }
+
 resource "github_repository_file" "ga_workflow_main_updates" {
+  for_each            = local.file_map
   repository          = "terraform-azurerm-avm-res-documentdb-databaseaccount"
   branch              = "main" # or whichever branch you want to add the file to
-  file                = ".github/workflows/avmupgrades_update_dependabot.yml"
-  content             = file("${path.module}/../files/avmupgrades_update_dependabot.yml")
+  file                = each.key
+  content             = base64decode(each.value)
   commit_message      = "[AVMUpgrades] Add GitHub Action workflow to update main branch"
   commit_author       = "Terraform"
   commit_email        = local.commit_email
